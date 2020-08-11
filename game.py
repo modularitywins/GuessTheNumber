f255a57931b2a3b1133c7cbd99c2441a772b36b3
Modularity Wins <modularity.wins@outlook.com>
2020-08-11 20:31:32 +0100

    Implement the logic to play a single round

--- game.py
+++ game.py
@@ -1,7 +1,22 @@
+import random
+from utils import *
+
 def play_game():
     print("Starting a new round...")
     # generate a random target, and remember this target
+    TARGET = random.randint(1, 100) # 1 <= target <= 100
+    print("A random integer between 1 and 100 (both inclusive) is generated")
     # let the user keep guessing until right (while)
-    # inside: ask for guess,
-    #           verify the guess
-    pass
+    while True:
+        # ask for the guess
+        user_guess = input("Please take a guess: ").strip() # strip(): clean the white space at the beginning and end of a string
+        # make sure the user input is a number
+        if not user_guess.isdigit():
+            print("Invalid guess: you should enter a number")
+            continue
+        user_guess = int(user_guess)
+        # verify the guess
+        is_guessed_correct = check_guess(user_guess, TARGET) # either true or false
+        # if guess is correct -> break out of the loop
+        if is_guessed_correct:
+            break
