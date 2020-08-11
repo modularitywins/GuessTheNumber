a8edcfc1a9bd7ecc1ef1ee05447232535fd07cc3
Modularity Wins <modularity.wins@outlook.com>
2020-08-11 20:38:28 +0100

    Implement the logic for multi-round game

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ from game import *
 def main():
     print("Welcome to Guess the Number")
     play_game()
-    # while: ask the user if he wants to play another round
-    # only exit when the user answers NO
+    while True:
+        user_input = input("Do you want to play another round [y/n]: ")
+        if user_input.lower() == "n":
+            break;
+        play_game()
     print("We are done with this game. Exiting...")
 
 main()
