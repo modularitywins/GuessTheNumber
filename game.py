c6b64b6f4bf1470b859cd2ee7f23908db9ae5000
Modularity Wins <modularity.wins@outlook.com>
2020-08-11 19:34:48 +0100

    Write the rought design for this project (play a single round)
    Once we implement the "single round", we can easily implement the multi-round game logic.

---
+++ game.py
@@ -0,0 +1,7 @@
+def play_game():
+    print("Starting a new round...")
+    # generate a random target, and remember this target
+    # let the user keep guessing until right (while)
+    # inside: ask for guess,
+    #           verify the guess
+    pass
---
+++ main.py
@@ -0,0 +1,10 @@
+from game import *
+
+def main():
+    print("Welcome to Guess the Number")
+    play_game()
+    # while: ask the user if he wants to play another round
+    # only exit when the user answers NO
+    print("We are done with this game. Exiting...")
+
+main()
